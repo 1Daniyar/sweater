56b5c44d3f1c35110dce6f6cfa8d936d86ab738b
1Daniyar <66485123+1Daniyar@users.noreply.github.com>
2021-04-01 10:21:15 +0300

    Delete target directory

--- target/test-classes/messages-list-before.sql
+++
@@ -1,9 +0,0 @@
-delete from message;
-
-insert into message(id, text, tag,  user_id) values
-(1, 'first mes', 'my tag', 1),
-(2, 'sec mes', 'sec', 1),
-(3, 'third mes', 'my tag', 1),
-(4, 'forth mes', 'forth', 1);
-
-alter sequence hibernate_sequence restart with 10;
